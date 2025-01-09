b329b65e755bb0cdfb884a1c8e1ab14cdfa468e3
wafflesJ <jacobsamuelmills7@gmail.com>
2025-01-09 08:44:16 -0500

    Update cmd.js port

--- cmd.js
+++ cmd.js
@@ -5,7 +5,7 @@ const CMD = require('commander');
 
 CMD.version('0.0.1')
     .usage('[options] <directory entry point> \n\r  By providing entry directory rendering server can find requested JSX file by URL path/name')
-    .option('-p, --port <n>', 'Define Server Port for starting proxy rendering server', function(val) {return parseInt(val)}, 8888)
+    .option('-p, --port <n>', 'Define Server Port for starting proxy rendering server', function(val) {return parseInt(val)}, 3000)
     .option('-R, --release', 'Run rendering in release mode, which means server will make a caching and a lot more!', function(val) { return !val; })
     .option('-w, --watch', 'Watch for file changes for hot reloading rendering process during development', function(val) { return !val })
     .parse(process.argv);
